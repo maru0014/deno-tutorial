bf87fd4b0ad62f8d681116a4496d00753ff6be65
maru0014 <maru0014@gmail.com>
2023-05-08 17:44:17 +0900

    Add Input Form

--- index.ts
+++ index.ts
@@ -2,7 +2,12 @@ import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 
 const handler = async (request: Request): Promise<Response> => {
   const requestUrl = new URL(request.url);
-  const pokemonNumber = requestUrl.searchParams.get("number");
+  let pokemonNumber = requestUrl.searchParams.get("number");
+
+  // パラメータが未入力の場合は25とする
+  if (!pokemonNumber) {
+    pokemonNumber = "25";
+  }
 
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`;
   const resp = await fetch(url, {
@@ -11,10 +16,23 @@ const handler = async (request: Request): Promise<Response> => {
     },
   });
 
-  let body = `<p>Not Found. number: ${pokemonNumber}`;
+  let body = `
+    <form>
+      <label for="number">ポケモンのナンバー: </label>
+      <input type="number" id="number" name="number" value="${pokemonNumber}" min="1" max="893">
+      <input type="submit" value="検索">
+    </form>
+    <br>
+    <p>Not Found. number: ${pokemonNumber}`;
   if (resp.status === 200) {
     const json = await resp.json();
     body = `
+      <form>
+        <label for="number">Pokemon Number: </label>
+        <input type="number" id="number" name="number" value="${pokemonNumber}" min="1" max="893">
+        <input type="submit" value="GET">
+      </form>
+      <br>
       <p>Id: ${json["id"]}</p>
       <p>Name: ${json["name"]}</p>
       <img src="${json["sprites"]["front_default"]}" alt="${json["name"]}">
